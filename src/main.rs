b96f050b8e23b3f8574555ee4f24f79ea1700d55
jack-dinsmore <goodsirmango@gmail.com>
2022-04-23 20:36:47 -0400

    Font rendering started

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@ mod graphics;
 mod util;
 
 use glow::*;
-use std::str;
 use graphics::shaders::{ShaderManager, Shader};
+use glow_glyph::{ab_glyph, GlyphBrushBuilder, Section, Text, Region};
 
-const VERTEX_SHADER_SOURCE: &str = &include_str!("graphics/shaders/vertex.hlsl");
-const FRAGMENT_SHADER_SOURCE: &str = &include_str!("graphics/shaders/fragment.hlsl");
 
 fn main() {
     unsafe {
@@ -15,33 +13,61 @@ fn main() {
 
         // Create a shader program from source
         let shader_manager = ShaderManager::new(&gl);
-        shader_manager.load_example();
 
         // Upload uniforms
         shader_manager.set_uniforms(Shader::Example(0.8));
 
+        // Prepare glyph_brush
+        let inconsolata = ab_glyph::FontArc::try_from_slice(include_bytes!(
+            "Inconsolata-Regular.ttf"
+        )).expect("Could not open font file");
+        let mut glyph_brush = GlyphBrushBuilder::using_font(inconsolata).build(&gl);
+
         // Create a vertex buffer and vertex array object
         let (vbo, vao) = create_vertex_buffer(&gl);
 
+        gl.enable(glow::FRAMEBUFFER_SRGB);
+        gl.enable(glow::BLEND);
+        gl.blend_func(glow::SRC_ALPHA, glow::ONE_MINUS_SRC_ALPHA);
         gl.clear_color(0.1, 0.2, 0.3, 1.0);
 
         'render: loop {
-            {
-                for event in events_loop.poll_iter() {
-                    if let sdl2::event::Event::Quit { .. } = event {
-                        break 'render;
-                    }
+            for event in events_loop.poll_iter() {
+                match event {
+                    sdl2::event::Event::Quit{..} => break 'render,
+                    _ => ()
                 }
             }
 
-            gl.clear(glow::COLOR_BUFFER_BIT);
+            gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
+
+
+            shader_manager.load_example();
             gl.draw_arrays(glow::TRIANGLES, 0, 3);
+
+            // Queue text to be drawn
+            glyph_brush.queue(Section {
+                screen_position: (30.0, 30.0),
+                bounds: (1024.0, 769.0),
+                text: vec![Text::default()
+                    .with_text("Hello glow_glyph!")
+                    .with_color([0.0, 0.0, 0.0, 1.0])
+                    .with_scale(40.0)],
+                ..Section::default()
+            });
+
+            // Draw text
+            //glyph_brush.draw_queued(&gl, 1024, 769).expect("Draw queued");
+
+            shader_manager.load_example();
+            gl.draw_arrays(glow::TRIANGLES, 0, 3);
+
             window.gl_swap_window();
         }
 
         // Clean up
         gl.delete_vertex_array(vao);
-        gl.delete_buffer(vbo)
+        gl.delete_buffer(vbo);
     }
 }
 
